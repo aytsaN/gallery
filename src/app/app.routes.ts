a5d3f50e26f42613ea167d1afbf3ba6069ae8fff
Anastasiya_Shamal <anastasiya_shamal@epam.com>
2026-01-28 17:09:17 +0100

    refactor: add lazy loading to app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,17 @@
 import { Routes } from '@angular/router';
 
 import { PhotoPage } from './photo-page/photo-page';
-import { FavoritesPage } from './favorites-page/favorites-page';
-import { PhotoDetailsPage } from './photo-details-page/photo-details-page';
 
 export const routes: Routes = [
   { path: '', component: PhotoPage },
-  { path: 'favorites', component: FavoritesPage },
-  { path: 'photos/:id', component: PhotoDetailsPage },
+  {
+    path: 'favorites',
+    loadComponent: () => import('./favorites-page/favorites-page').then(m => m.FavoritesPage),
+  },
+  {
+    path: 'photos/:id',
+    loadComponent: () =>
+      import('./photo-details-page/photo-details-page').then(m => m.PhotoDetailsPage),
+  },
   { path: '**', redirectTo: '' },
 ];
